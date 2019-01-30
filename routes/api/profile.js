d21db4e3ab6ff102972833f9b4ac72005c75b9cf
Marizu-Ibewiro Makozi <marizumakozi97@gmail.com>
2019-01-30 11:27:32 +0100

    added routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,1 +1,7 @@
- 
+const express = require('express');
+const router= express.Router();
+
+router.get('/test', (req, res)=> res.json({msg: "Profile is working"})) ;
+
+module.exports= router;
+
--- server.js
+++ server.js
@@ -17,6 +17,13 @@ mongoose
 
 app.get("/", (req, res) => res.send("hello makozi and mum  "));
 
+//use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = process.env.port || 5000;
 
 app.listen(port, () => console.log("Server running at port  5000 ${post}"));
+
+
